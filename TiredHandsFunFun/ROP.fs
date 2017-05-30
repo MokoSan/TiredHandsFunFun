4271cb3f6df7fb72ea23e4d0faf3d4b4b8cb39ba
MokoSan <mukund.sharma92@gmail.com>
2017-05-29 22:12:05 -0400

    Added a bunch of preliminary functions to the ROP module

--- TiredHandsFunFun/ROP.fs
+++ TiredHandsFunFun/ROP.fs
@@ -4,3 +4,20 @@
 type Result<'TSuccess, 'TFailure> = 
     | Success of 'TSuccess
     | Failure of 'TFailure
+
+let either successTrack failureTrack twoTrackInput = 
+    match twoTrackInput with
+    | Success s -> successTrack s
+    | Failure f -> failureTrack f  
+
+let fail input =
+    Failure input
+
+let success input = 
+    Success input 
+
+let bind switchFunction = 
+    either fail switchFunction 
+
+let (>>=) switchFunction twoTrackInput = 
+    bind switchFunction twoTrackInput
